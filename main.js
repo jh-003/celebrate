f845f4d1b7848fb196b22195802780a98dcf447b
jeeho-choi <jeehochoi003@gmail.com>
2024-07-24 20:07:15 +0900

    add files

---
+++ main.js
@@ -0,0 +1,17 @@
+// RSVP.js
+// function showRSVP() {
+//     document.getElementById('rsvp').style.display = 'block';
+// }
+
+// function hideRSVP() {
+//     document.getElementById('rsvp').style.display = 'none';
+// }
+
+// document.getElementById('rsvp-form').addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     alert('Thank you for your RSVP!');
+//     hideRSVP();
+// });
+
+
+
