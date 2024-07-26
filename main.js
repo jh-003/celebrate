75d01fa7a1997a35c87252c5762645e6e6491c9b
jeeho-choi <jeehochoi003@gmail.com>
2024-07-26 23:04:16 +0900

    add svg and goggle map

--- main.js
+++ main.js
@@ -60,5 +60,41 @@
         }
         //seconds
       }, 0);
+    
+
   })();
-  
+
+// window.initMap = function () {
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 10,
+//   });
+// };
+
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: 40.747434, lng: -73.64539909999999 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 15,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "The Jericho Terrace",
+  });
+}
+
+initMap();
